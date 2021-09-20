bba4d3a71634ef926193df7abeef2a9a6aa0f472
gurunandan.rao@oracle.com <gurunandan.rao@oracle.com>
2021-09-20 12:37:49 +0530

    JDK 11 build.
    Signed-off-by: gurunandan.rao@oracle.com <gurunandan.rao@oracle.com>

--- docker/build_ditck.sh
+++ docker/build_ditck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #
-# Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2019, 2021 Oracle and/or its affiliates. All rights reserved.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License v. 2.0, which is available at
@@ -14,6 +14,9 @@
 #
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  
+export JAVA_HOME=${JDK11_HOME}
+export PATH=$JAVA_HOME/bin:$PATH
+
 echo "ANT_HOME=$ANT_HOME"
 echo "export JAVA_HOME=$JAVA_HOME"
 echo "export MAVEN_HOME=$MAVEN_HOME"
